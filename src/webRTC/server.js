8aca91e2f89d0da3a64d68acf7e18f834e60c020
Shobhiti7 <shobhit.sharma@i7infomedia.com>
2024-05-09 18:16:26 +0530

    fix

--- src/webRTC/server.js
+++ src/webRTC/server.js
@@ -2,6 +2,11 @@ export const server={
     iceServers:[
     {
         urls:['stun:stun2.l.google.com:19302','stun:stun1.l.google.com:19302']
-    }
+    },
+    {
+        url: 'turn:numb.viagenie.ca',
+        credential: 'muazkh',
+        username: 'webrtc@live.com'
+    },
 ]
 }
