d0a8088b5b10d43b1c287ee5cc946307f8f322fe
Shobhiti7 <shobhit.sharma@i7infomedia.com>
2024-05-09 23:01:27 +0530

    fix

--- src/webRTC/createoffer.js
+++ src/webRTC/createoffer.js
@@ -1,5 +1,30 @@
 export async function create_offer_remote(peerConnection, server, ws, local_video_ref, remote_video_ref, selectedUser) {
-    let Connection = new RTCPeerConnection(server);
+    let Connection = new RTCPeerConnection({iceServers: [
+        {
+          urls: "stun:stun.relay.metered.ca:80",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80",
+          username: "5bc265146469de02b6e0324c",
+          credential: "MZTnljpPqcgC47cs",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80?transport=tcp",
+          username: "5bc265146469de02b6e0324c",
+          credential: "MZTnljpPqcgC47cs",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:443",
+          username: "5bc265146469de02b6e0324c",
+          credential: "MZTnljpPqcgC47cs",
+        },
+        {
+          urls: "turns:global.relay.metered.ca:443?transport=tcp",
+          username: "5bc265146469de02b6e0324c",
+          credential: "MZTnljpPqcgC47cs",
+        },
+    ],
+  });
     peerConnection.current = Connection;
     let localstream = local_video_ref.current.srcObject;
     let remotestream = new MediaStream();
