395d015a810a777936247fddbb0ffda6b80e6073
Shobhiti7 <shobhit.sharma@i7infomedia.com>
2024-05-09 18:27:40 +0530

    fix

--- src/webRTC/server.js
+++ src/webRTC/server.js
@@ -1,12 +1,19 @@
-export const server={
-    iceServers:[
-    {
-        urls:['stun:stun2.l.google.com:19302','stun:stun1.l.google.com:19302']
-    },
-    {
-        url: 'turn:numb.viagenie.ca',
-        credential: 'muazkh',
-        username: 'webrtc@live.com'
-    },
-]
-}
+export const iceServers = {
+    iceServers: [
+        {
+            urls: ["stun:bn-turn1.xirsys.com"]
+        },
+        {
+            username: "FwxuJ06cV6DyjY4jTXpfTJ5pYlGm01NTxryKwV8GipmlrfKxg9yN6S-gh9B5cG-FAAAAAGY8xvpzaG9iaGl0Mjg=",
+            credential: "f3820fba-0e02-11ef-9559-0242ac140004",
+            urls: [
+                "turn:bn-turn1.xirsys.com:80?transport=udp",
+                "turn:bn-turn1.xirsys.com:3478?transport=udp",
+                "turn:bn-turn1.xirsys.com:80?transport=tcp",
+                "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+                "turns:bn-turn1.xirsys.com:443?transport=tcp",
+                "turns:bn-turn1.xirsys.com:5349?transport=tcp"
+            ]
+        }
+    ]
+};
