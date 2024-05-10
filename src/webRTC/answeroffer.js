59eee4c07a7ca16439cb0688743cb0f08febc269
Shobhiti7 <shobhit.sharma@i7infomedia.com>
2024-05-10 15:15:12 +0530

    fix

--- src/webRTC/answeroffer.js
+++ src/webRTC/answeroffer.js
@@ -1,14 +1,6 @@
 export async function answer_offer_remote(peerConnection,server,ws,local_video_ref,remote_video_ref,remote_user,offer_data){
-    const response = 
-    await fetch("https://speechsphere.metered.live/api/v1/turn/credentials?apiKey=028fad1b2daad62402a032033aa82b2178d5");
   
-  // Saving the response in the iceServers array
-  const iceServers = await response.json();
-  
-  // Using the iceServers array in the RTCPeerConnection method
-  var Connection = new RTCPeerConnection({
-    iceServers: iceServers
-  });
+  var Connection = new RTCPeerConnection(server)
     peerConnection.current=Connection
     let localstream = local_video_ref.current.srcObject;
     let remotestream = new MediaStream();
