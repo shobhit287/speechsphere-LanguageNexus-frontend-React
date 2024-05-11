5a134b50dec86c849d07d0d63ea31dfe77544055
Shobhiti7 <shobhit.sharma@i7infomedia.com>
2024-05-11 12:43:48 +0530

    fixz

--- src/webRTC/server.js
+++ src/webRTC/server.js
@@ -1,10 +1,27 @@
 export const server = {
   iceServers: [
-    { urls: "stun:relay1.expressturn.com:3478" },
-    { 
-      urls: "turn:relay1.expressturn.com:3478",
-      username: "efYHBIPJD0ITZU8EEZ",
-      credential: "nVsTRbmY3mgOq55g"
-    }
-  ]
+    {
+      urls: "stun:stun.relay.metered.ca:80",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80",
+      username: "9c5d1a0fc5d641f9c1b6c657",
+      credential: "DqZp8UeIDGqx/X9s",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80?transport=tcp",
+      username: "9c5d1a0fc5d641f9c1b6c657",
+      credential: "DqZp8UeIDGqx/X9s",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:443",
+      username: "9c5d1a0fc5d641f9c1b6c657",
+      credential: "DqZp8UeIDGqx/X9s",
+    },
+    {
+      urls: "turns:global.relay.metered.ca:443?transport=tcp",
+      username: "9c5d1a0fc5d641f9c1b6c657",
+      credential: "DqZp8UeIDGqx/X9s",
+    },
+],
 };
