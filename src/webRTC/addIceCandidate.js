4538634ce427fda87bce32c4aad8b1b4a1e5dd3a
Shobhiti7 <shobhit.sharma@i7infomedia.com>
2024-05-24 14:38:15 +0530

    fix

--- src/webRTC/addIceCandidate.js
+++ src/webRTC/addIceCandidate.js
@@ -1,5 +1,4 @@
 export const addCandidate=async (peerConnection,candidate)=>{
-    console.log("INSiDE THIS")
     const ice_candidate = new RTCIceCandidate(candidate);
     await peerConnection.current.addIceCandidate(ice_candidate);
 }
