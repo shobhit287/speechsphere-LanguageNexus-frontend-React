0f5a88f0240a93e01a5422cd059c85252a6f6bd5
Shobhiti7 <shobhit.sharma@i7infomedia.com>
2024-05-24 13:59:26 +0530

    fix

--- src/webRTC/acceptanswer.js
+++ src/webRTC/acceptanswer.js
@@ -2,7 +2,7 @@ export async function accept_answer(peerConnection,accept_sdp){
 
     if(!peerConnection.current.currentRemoteDescription)
     {
-        
+        console.log("FINAL SDP",accept_sdp)
          peerConnection.current.setRemoteDescription(accept_sdp);
 
         
